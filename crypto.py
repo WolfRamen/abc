64a2d45838b42a95a440d1da6aa479e72a0e8d9c
Amogus <wolfram.reglich@gmail.com>
2021-10-19 15:27:41 +0200

    first commit

---
+++ crypto.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import requests 
+import json
+import time
+import numpy
+crypto_url = "https://api.cryptonator.com/api/ticker/eth-eur"
+#cryptos = ["btc", "eth"]
+
+headers = {
+	'User-Agent': 'My User Agent 1.0'
+}
+def cryptokurs():
+	kurs = float(json.loads(requests.request("GET", crypto_url, headers=headers).text)["ticker"]["price"])
+	return kurs
+kurs = cryptokurs()
+f = open("/home/wolfram/crypto", "w")
+f.write("kurs = " + str(kurs))
+f.close()
+#file.write(kurs)
+while True:
+	kurs = cryptokurs()
+	print(kurs)
+	time.sleep(10)
